c46ccd225d3f289d31fe705e46f949b09bfa9d11
ugougo05 <ugoanaka@gmail.com>
2025-04-16 13:42:15 +0200

    initial commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   type        = string
   description = "Azure region"
-  default     = "westeurope"
+  default     = "Central US"
 }
 
 variable "aks_cluster_name" {
